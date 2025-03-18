0cb29c16af57976113ead304aecd87c7b42a61c3
salah <salaheddineabaid@gmail.com>
2025-03-18 14:35:38 +0000

    Initial commit

---
+++ src/main/java/ma/emsi/charitywebapp/CharityWebAppApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.charitywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/emsi/charitywebapp/CharityWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.charitywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
